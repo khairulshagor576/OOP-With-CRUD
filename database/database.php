8d2930c9eea423337917376ca5b0a32f4d4f45b7
Mohammad Khairul Islam <khairulsagor567@gmail.com>
2021-06-11 00:04:36 +0600

    a database file create and init config and database file here and database connection create in database.php

--- database/database.php
+++ database/database.php
@@ -6,6 +6,19 @@ class Database
     public $user   = DB_USER;
     public $pass   = DB_PASS;
     public $dbname = DB_NAME;
+
+    public $link;
+    public $error;
+
+    private function connectionDB()
+    {
+        $this->link= new mysqli($this->host,$this->user,$this->pass,$this->dbname);
+        if(!$this->link)
+        {
+          $this->error="Connection Failed!".$this->link->connect_error;
+          return false;
+        }
+    }
 }
 
 
